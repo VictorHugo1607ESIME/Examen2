4038592d8f510e5ffc8af31f47f7303f42e0c97b
Ingenio01MX <138320261+Ingenio01MX@users.noreply.github.com>
2023-11-29 21:29:54 -0600

    Initial Commit

---
+++ Examen2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examen2
+//
+//  Created by Victor Hugo Martinez Ramirez on 29/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
